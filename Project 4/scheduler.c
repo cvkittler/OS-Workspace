9f00bef9400f082a5691543190155e928c1fa012
cvkittler <cvkittler@wpi.edu>
2021-03-11 15:41:21 -0500

    working on project 4

--- Project 4/scheduler.c
+++ Project 4/scheduler.c
@@ -2,6 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+struct job_
+{
+    int id;
+    int legth;
+    int amoutDone;
+    struct job *next;
+}job_;
+
+
 int main(int argc, char **argv){
 
     printf("Hello, please help me schedule!\n");
@@ -25,7 +35,42 @@ int main(int argc, char **argv){
     }else if(policy[0] == 'R'){
         policyFlag = 3;
     }
-
+    //parse the time slice into an int
     int timeSliceInt= atoi(timeSlice);
-    printf("%d\n",timeSliceInt);
+    //read the job
+    FILE *fp;
+    fp = fopen(jobPath, "r");
+    char buffer[255];
+    int jobs[50];
+    int counter = 0;
+    for(int i = fscanf(fp,"%s",buffer); i != EOF; i = fscanf(fp,"%s",buffer)){
+        jobs[counter] = atoi(buffer);
+        counter = counter + 1;
+    }
+    //setup the workflow
+    switch (policyFlag)
+    {
+    case 1://First in first out
+        struct job_ * firstJob;
+        (*firstJob).id = 0;
+        (*firstJob).legth = jobs[firstJob->id];
+        (*firstJob).amoutDone = 0;
+        (*firstJob).next = (void*)NULL;
+        struct job_ * lastJob = firstJob;
+        for(int i = 1; i < counter; i ++){
+            struct job_ * currJob;
+            (*currJob).id = i;
+            (*currJob).legth = jobs[currJob->id];
+            (*currJob).amoutDone = 0;
+            (*currJob).next = (void*)NULL;
+            (*lastJob).next = currJob;
+        }
+        break;
+    case 2://Shortest job first
+        break;
+    case 3://Round roben
+        break;
+    default:
+        break;
+    }
 }
